500587e4bc40b371938d306b0185ab3ca24ee8aa
Patricia Bernasconi <bernasconi.patricia@gmail.com>
2018-07-03 22:40:16 +0200

    specific visuals

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,10 @@ class PagesController < ApplicationController
   # skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    @lembeh = Visual.where("name LIKE?", "%lembeh%")
+    @athens = Visual.where("name LIKE?", "%athens%")
+    @athens = Visual.where("name LIKE?", "%athens%")
+    @ios = Visual.where("name LIKE?", "%ios%")
+
   end
 end
